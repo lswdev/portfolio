5886e0be11beac982d713aa0f33b0e88f0f54cdb
lswsepia <lswsepia@gmail.com>
2024-05-03 18:08:27 +0900

    각종 코드 추가. 스크롤 이벤트 예정

--- src/views/IntroDetailView.vue
+++ src/views/IntroDetailView.vue
@@ -1,28 +1,26 @@
 <!--eslint-disable-->
 <template>
-  <section class="detail-section" id="detailSection">
-    <div class="main-top-contain detail-contain" id="detail-contain">
+  <section class="detail-section">
+    <div class="main-top-contain detail-contain" id="detail-contain" ref="detailContain1">
       <div class="intro-detail">
-        <div class="login">
-          <h2>Login</h2>
-        </div>
+<!--          <Carousel></Carousel>-->
       </div>
     </div>
-    <div class="main-top-contain detail-contain" id="detail-contain2">
+    <div class="main-top-contain detail-contain" id="detail-contain2" ref="detailContain2">
       <div class="intro-detail">
         <div class="login">
           <h2>Login2</h2>
         </div>
       </div>
     </div>
-    <div class="main-top-contain detail-contain" id="detail-contain3">
+    <div class="main-top-contain detail-contain" id="detail-contain3" ref="detailContain3">
       <div class="intro-detail">
         <div class="login">
           <h2>Login3</h2>
         </div>
       </div>
     </div>
-    <div class="main-top-contain detail-contain" id="detail-contain4">
+    <div class="main-top-contain detail-contain" id="detail-contain4" ref="detailContain4">
       <div class="intro-detail">
         <div class="login">
           <h2>Login4</h2>
@@ -31,42 +29,21 @@
     </div>
   </section>
 </template>
-
-<script setup>
+<script>
 import { inputAnimation } from "@/mixins/mixins";
 
 export default {
-  methods:{
-    scrollPage(e) {
-      console.log(e);
-      const mSection = window.$(".detail-section");
-      let page = 1;
-
-      // wheel 기능 제거
-      window.addEventListener("wheel", function(e){
-        e.preventDefault();
-      },{passive : false});
-
-      mSection.animate({scrollTop : 0},10);
-
-      window.$(window).on("wheel", function(e) {
-        console.log(mSection.is(":animated"));
-        if(mSection.is(":animated")) return;
-        if(e.originalEvent.deltaY > 0) {
-          if(page == 4) return;
-          page++;
-        } else if(e.originalEvent.deltaY < 0) {
-          if(page == 1) return;
-          page--;
-        }
-        var posTop =(page-1) * window.$(window).height();
-        mSection.animate({scrollTop : posTop});
-      });
-    }
-  },
   mounted() {
     inputAnimation();
-    // this.scrollPage();
+    document.addEventListener('scroll', this.scrollEvents, true);
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.scrollEvents);
+  },
+  methods : {
+    scrollEvents(e) {
+      console.log("스크롤 될 때마다 이 함수 실행",e)
+    }
   },
 }
 </script>
