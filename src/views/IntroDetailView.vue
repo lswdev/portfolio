4b3cf121614584791a0d70c35248fce325d192e9
lswdev <lswdev@gmail.com>
2024-05-10 23:01:55 +0900

    nav list 추가

--- src/views/IntroDetailView.vue
+++ src/views/IntroDetailView.vue
@@ -1,46 +1,66 @@
 <!--eslint-disable-->
 <template>
-  <section class="detail-section" @wheel="scrolls" id="detail-section">
-    <div class="main-top-contain detail-contain" id="detail-contain1" ref="detailContain1"
-      style="border-bottom: 1px solid red">
-      <div class="my-avatar">
-        이시우<br>
-        1997. 02. 28<br>
-        Lee Siwoo
-      </div>
-      <span class="avatar-label">Developer</span>
+  <div>
+    <section class="detail-section" @wheel="scrolls" id="detail-section">
+      <!-- navList -->
+      <span class="detail-nav">
+        <div class="row cf">
+          <div class="three col">
+            <div class="hamburger" id="hamburger" @click="navList">
+              <span class="line"></span>
+              <span class="line"></span>
+              <span class="line"></span>
+            </div>
+          </div>
+        </div>
+        <ul class="nav-list" style="color: #FFF;" v-if="toggleNav">
+          <li>introduce</li>
+          <li>skill</li>
+          <li>careers</li>
+        </ul>
+      </span>
 
-      <img class="mouse-scroll" src="../assets/images/mouse-cursor-lg.png" alt="">
-    </div>
-    <div class="main-top-contain detail-contain" id="detail-contain2" ref="detailContain2">
-      <div class="intro-detail">
-        <div class="login">
-          <h2>Login2</h2>
+      <div class="main-top-contain detail-contain" id="detail-contain1" ref="detailContain1"
+        style="border-bottom: 1px solid red">
+        <div style="display: flex">
+          <div class="my-avatar">이시우<br>1997. 02. 28<br>Lee Siwoo</div>
+        </div>
+        <span class="avatar-label">Developer</span>
+        <img class="mouse-scroll" src="../assets/images/mouse-cursor-lg.png" alt="">
+      </div>
+      <div class="main-top-contain detail-contain" id="detail-contain2" ref="detailContain2">
+        <div class="intro-detail">
+          <div class="login">
+            <h2>Login2</h2>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="main-top-contain detail-contain" id="detail-contain3" ref="detailContain3">
-      <div class="intro-detail">
-        <div class="login">
-          <h2>Login3</h2>
+      <div class="main-top-contain detail-contain" id="detail-contain3" ref="detailContain3">
+        <div class="intro-detail">
+          <div class="login">
+            <h2>Login3</h2>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="main-top-contain detail-contain" id="detail-contain4" ref="detailContain4">
-      <div class="intro-detail">
-        <div class="login">
-          <h2>Login4</h2>
+      <div class="main-top-contain detail-contain" id="detail-contain4" ref="detailContain4">
+        <div class="intro-detail">
+          <div class="login">
+            <h2>Login4</h2>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+
+  </div>
 </template>
+
 <script>
 
 export default {
   data:() => ({
     windowTop: window.top.scrollY,
     windowHeight: null,
+    toggleNav: false,
   }),
   mounted() {
     window.scrollTo(0, 0);
@@ -58,7 +78,9 @@ export default {
       // 스크롤 DOWN
       if (e.wheelDelta < 0) {
         console.log("this.windowTop", this.windowTop);
-        if (this.windowTop === 0) {
+        if (this.windowTop >= 0) {
+          window.$('.mouse-scroll').css('opacity', '0');
+        } else if (this.windowTop >= 200) {
           window.$('.mouse-scroll').css('opacity', '0');
         }
         if (e.target.id === `detail-contain${sectionEl.children.length}`) {
@@ -73,6 +95,10 @@ export default {
       }
       // 맨 위로
       // window.scrollTo({top: 0, behavior:'smooth'});
+    },
+    navList() {
+      window.$('.hamburger').toggleClass('is-active');
+      this.toggleNav = !this.toggleNav;
     },
   },
 }
