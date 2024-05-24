9031459963713ca0a65795782c900a5a15f7d58b
lswsepia <lswsepia@gmail.com>
2024-05-24 18:46:35 +0900

    skill  /  career 리스트 등 DataList 추가

---
+++ src/DataList.js
@@ -0,0 +1,63 @@
+
+const skillList = [
+  {
+    skill: 'Vue',
+    desc: [
+      'vuex 사용 경험이 있습니다.',
+      'Vue2 option api를 사용할 수 있습니다.',
+      'Params를 활용한 동적 라우트 매칭이 가능합니다.',
+      '컴포넌트를 이용해 재사용성을 높힐 수 있습니다.',
+    ]
+  }, {
+    skill: 'React',
+    desc: [
+      'ReactHook 사용 경험이 있습니다.',
+      'yarn, npm 사용 경험이 있습니다.'
+    ]
+  }, {
+    skill: 'HTML/CSS',
+    desc: [
+      '웹표준을 지키려 노력합니다.',
+      'SCSS 문법에 익숙합니다.',
+      '반응형 웹 구현이 가능합니다.',
+      '크로스 브라우징에 대응할 수 있습니다.',
+      'Bootstrap, BootstrapVue 등의 css 라이브러리를 사용할 수 있습니다.',
+      'keyframe을 활용한 애니메이션 기법을 활용할 수 있습니다.',
+      '가독성을 위한 시멘틱 구조에 맞게 작성할 수 있습니다.',
+    ]
+  }, {
+    skill: 'Javascript',
+    desc: [
+      'ES6+ 문법에 익숙합니다.',
+      'Dataset을 이용한 Dom 사용이 가능합니다.',
+      'jQuery를 활용해 DOM을 제어하고 Ajax로 데이터를 가져올 수 있습니다.',
+      'async-await 비동기 문법을 사용할 수 있습니다. ',
+      'React와 Vue를 활용해 SPA의 상태를 관리할 수 있습니다.',
+      'Web Animation API를 이용한 다양한 애니메이션을 구현할 수 있습니다.'
+    ]
+  }, {
+    skill: 'Infra',
+    desc: [
+      'Kubernetes nginx 서버 배포 및 플러그인 적용 경험이 있습니다.',
+    ]
+  }, {
+    skill: 'ETC',
+    desc: [
+      'Git, GitHub, GitLab, SVN, Jenkins를 사용한 경험이 있습니다.',
+      'Figma, AdobeXD 디자인툴 협업 경험이 있습니다.',
+      'Postman을 이용한 Require 테스트 경험이 있습니다.',
+      'Swagger 라이브러리를 사용한 API 테스트 경험이 있습니다.',
+    ]
+  },{
+    skill: '',
+    desc: ['']
+  },
+]
+
+const careerList = [
+  {
+    skill: 'Vue',
+    desc: ['vuex 사용 경험이 있습니다.', 'Vue2 option api를 사용할 수 있습니다.']
+  },
+]
+export { skillList, careerList }
