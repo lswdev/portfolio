2f4198bab7dc13b237e29614cb69bfd0384a3acd
lswsepia <lswsepia@gmail.com>
2024-06-11 17:03:32 +0900

    cursor mobile device 처리중

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -17,9 +17,30 @@
 
 <script>
 import { inputAnimation } from "@/mixins/mixins";
+import { reactive } from "vue";
 
 export default {
+  data: () => ({
+    browserInfo: reactive({
+      platform: '',
+      userInfo: '',
+    }),
+    deviceInfo: '',
+  }),
   methods: {
+    // Mobile device 확인
+    isMobile() {
+      const info = navigator.userAgent;
+      let flag = false;
+
+      if( info.indexOf("iPhone") > -1
+          || info.indexOf("Android") > -1
+          || info.indexOf("iPad") > -1
+          || info.indexOf("iPod") > -1
+      ) { flag = true; }
+      this.deviceInfo = flag;
+      console.log(this.deviceInfo);
+    },
     pointerHideAnimation() {
       window.$('.cursor').fadeOut(200);
     },
@@ -33,9 +54,15 @@ export default {
     changePage() {
       this.$router.push('intro-detail');
     },
+    checkBrowser() {
+      this.browserInfo.platform = navigator.platform;
+      this.browserInfo.userInfo = navigator.userAgent;
+    },
   },
   mounted() {
     inputAnimation();
+    this.checkBrowser();
+    this.isMobile();
   },
 }
 </script>
